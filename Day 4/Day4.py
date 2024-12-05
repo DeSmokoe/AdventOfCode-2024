6196402585dcb3fd0e6ec74d23401c0eb9b8f27a
DeSmokoe <alexvandromme@gmail.com>
2024-12-05 19:40:54 +0100

    Namechange

---
+++ Day 4/Day4.py
@@ -0,0 +1,10 @@
+def readfile(filename):
+    with open(filename, "r") as f:
+        return f.read()
+
+
+def dataprep(filename):
+    my_file = readfile(filename)
+    data = my_file
+
+    return data
