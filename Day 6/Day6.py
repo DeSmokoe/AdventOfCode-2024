a7494f3de61f2d48f633dc136b9dc962e5c61948
DeSmokoe <alexvandromme@gmail.com>
2024-12-07 01:00:11 +0100

    Day 6 Complete

--- Day 6/Day6.py
+++ Day 6/Day6.py
@@ -10,140 +10,183 @@ def dataprep(filename):
 
     return data
 
+
 def find_starting_position(data):
-
     for i in range(len(data)):
-        for j in range(len(data[i])):
-            if data[i][j] == "^":
-                coordinates = (i, j)
-                direction = "up"
-                return coordinates, direction
-            elif data[i][j] == ">":
-                coordinates = (i, j)
-                direction = "right"
-                return coordinates, direction
-            elif data[i][j] == "v":
-                coordinates = (i, j)
-                direction = "down"
-                return coordinates, direction
-            elif data[i][j] == "<":
-                coordinates = (i, j)
-                direction = "left"
-                return coordinates, direction
+        if "^" in data[i]:
+            coordinates = (i, data[i].index("^"))
+            direction = "up"
+            return coordinates, direction
+
+        elif "v" in data[i]:
+            coordinates = (i, data[i].index("v"))
+            direction = "down"
+            return coordinates, direction
+
+        elif "<" in data[i]:
+            coordinates = (i, data[i].index("<"))
+            direction = "left"
+            return coordinates, direction
+
+        elif ">" in data[i]:
+            coordinates = (i, data[i].index(">"))
+            direction = "right"
+            return coordinates, direction
+    else:
+        print("No starting position found")
 
 
-def count_locations(filename):
-    completed_map = map_patrol_route(filename)
+def count_locations(filename, send_data=False):
+    data = dataprep(filename)
+    completed_map = map_patrol_route(data)
 
     # count amount of X's in the map
     count = 0
-    for i in range(len(completed_map)):
-        for j in range(len(completed_map[i])):
-            if completed_map[i][j] == "X":
+    for i in range(len(completed_map[0])):
+        for j in range(len(completed_map[0][i])):
+            if completed_map[0][i][j] == "X":
                 count += 1
 
-    return count
+    if send_data:
+        # change the original starting position back to the original direction
+        notation = "^"
+        if completed_map[3] == "up":
+            notation = "^"
+        elif completed_map[3] == "down":
+            notation = "v"
+        elif completed_map[3] == "left":
+            notation = "<"
+        elif completed_map[3] == "right":
+            notation = ">"
 
-def map_patrol_route(filename):
-    data = dataprep(filename)
-    (x, y), direction = find_starting_position(data)
-    repeating = False
-    history = [(x, y), direction]
-    temporary_counter = 0
+        completed_map[0][completed_map[2][0]][completed_map[2][1]] = notation
+
+        return count, completed_map[0]
+    else:
+        return count
+
+
+def map_patrol_route(data):
+    start_position, start_direction = find_starting_position(data)
+    (x, y) = start_position
+    direction = start_direction
+    history = []
 
     try:
-        while True and not repeating and temporary_counter < 1000:
-            data, x, y, direction, history = move_guard(data, x, y, direction, history)
-            # repeating = check_for_loop(history)
-            temporary_counter += 1
+        while 0 <= x < len(data) and 0 <= y < len(data[0]):
+            data, x, y, direction, history, loop_detected = move_guard(data, x, y, direction, history)
+            if loop_detected:
+                return data, loop_detected, start_position, start_direction
 
     except IndexError:
-        for i in range(len(data)):
-            print("".join(data[i]))
-        print("\n")
-        print("End of the map reached.")
+        return data, False, start_position, start_direction
 
-    return data
 
-def check_for_loop(history):
+def check_for_loop(current_position, history, counter):
+    counter += 1
+    # only check every 500 steps
+    if counter % 500 == 0:
+        return False
 
-    # check if history has any duplicates
-    for i in range(len(history)):
-        for j in range(len(history)):
-            # print(history[i], history[j])
-            if i != j and history[i] == history[j]:
-                print("Loop detected at", history[i])
-                return True
+    # check if current position has been visited before
+    if history.count(current_position) > 0:
+        return True
 
 
 def move_guard(data, x, y, direction, history):
-    if x == 31 and y == 121 and direction == "left":
-        print("Checkpoint reached")
+    loop_detected = False
+    counter = 0
 
     if direction == "up":
-
         # change every "." to "X" until you hit a "#"
-        while data[x][y] != "#":
+
+        while data[x][y] != "#" and data[x][y] != "0" or x < 0 or y < 0:
             if x < 0 or y < 0:
                 raise IndexError
-
             data[x][y] = "X"
             x -= 1
-            last_position = [(x, y), "up"]
-            history.append(last_position)
 
         x += 1
         data[x][y] = ">"
         direction = "right"
 
     elif direction == "right":
-
         # change every "." to "X" until you hit a "#"
-        while data[x][y] != "#":
+        while data[x][y] != "#" and data[x][y] != "0" or x < 0 or y < 0:
             if x < 0 or y < 0:
                 raise IndexError
             data[x][y] = "X"
             y += 1
-            last_position = [(x, y), "right"]
-            history.append(last_position)
 
         y -= 1
         data[x][y] = "v"
         direction = "down"
 
     elif direction == "down":
-
         # change every "." to "X" until you hit a "#"
-        while data[x][y] != "#":
+        while data[x][y] != "#" and data[x][y] != "0" or x < 0 or y < 0:
             if x < 0 or y < 0:
+                print("end of map")
                 raise IndexError
             data[x][y] = "X"
             x += 1
-            last_position = [(x, y), "down"]
-            history.append(last_position)
 
-        data[x-1][y] = "<"
+        data[x - 1][y] = "<"
         direction = "left"
         x -= 1
 
     elif direction == "left":
-
         # change every "." to "X" until you hit a "#"
-        while data[x][y] != "#":
+        while data[x][y] != "#" and data[x][y] != "0" or x < 0 or y < 0:
             if x < 0 or y < 0:
                 raise IndexError
             data[x][y] = "X"
             y -= 1
-            last_position = [(x, y), "left"]
-            history.append(last_position)
 
-        data[x][y+1] = "^"
+        data[x][y + 1] = "^"
         direction = "up"
         y += 1
 
-    return data, x, y, direction, history
+    last_position = [(x, y), direction]
+    if check_for_loop(last_position, history, counter):
+        loop_detected = True
+    history.append(last_position)
+
+    return data, x, y, direction, history, loop_detected
 
 
+# Part 1
 # print(count_locations("Test.txt"))
 # print(count_locations("Puzzle.txt"))
-print(count_locations("Puzzle.txt"))
+
+
+def find_creatable_loops(filename):
+    data = dataprep(filename)
+    loop_counter = 0
+
+    # get coordinates of all walked positions in an unaltered map
+    locations_amount, unaltered_locations = count_locations(filename, send_data=True)
+
+    # place one obstacle at a time in each position (except the starting position) and see if it creates a loop
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if unaltered_locations[i][j] == "X" and not data[i][j] == "^" and not data[i][j] == ">" and not data[i][j] == "v" and not data[i][j] == "<":
+
+                # print(f"Checking position {i}, {j}")
+                # create a copy of the data
+                new_data = [k.copy() for k in data]
+                new_data[i][j] = "0"
+                new_map_completed = map_patrol_route(new_data)
+
+                if new_map_completed[1]:
+                    loop_counter += 1
+                    print(f"Loop detected at position {i}, {j}")
+                    continue
+
+    print("\n")
+    print(f"Amount of loops detected: {loop_counter}")
+
+
+# Part 2
+# find_creatable_loops("Test.txt")
+find_creatable_loops("Puzzle.txt")
